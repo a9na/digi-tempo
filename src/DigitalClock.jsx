d02aadc5178e37535da9abdd69cc42a9241974f0
a9na <anaiscoding@gmail.com>
2024-08-09 17:20:39 +0200

    commit

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react'
+
+
+function DigitalClock(){
+
+
+    return( <div className='clock-container'>
+                <div className='clock'>
+
+                </div>
+            </div>);
+}
+
+
+export default DigitalClock;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
