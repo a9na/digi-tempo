44a07c9cc85c10a3e570847a9681407f7b118b55
a9na <anaiscoding@gmail.com>
2024-08-11 16:21:35 +0200

    commit

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -3,6 +3,13 @@ import React, {useState, useEffect} from 'react'
 
 function DigitalClock(){
 
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+    }, []);
 
     return( <div className='clock-container'>
                 <div className='clock'>
